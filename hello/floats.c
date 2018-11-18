de271d7f3b0b43a8006d4a17a8fee4b405903ecc
Rose Parker <parker.rose@ymail.com>
2018-11-18 23:29:10 +0000

    added .2 after % sign on answer for division and multiplication so that it returns only 2 places after the decimal point

--- hello/floats.c
+++ hello/floats.c
@@ -13,7 +13,7 @@ int main(void)
     //arithmetic
     printf("%f plus %f is %f\n", x, y, x + y);
     printf("%f minus %f is %f\n", x, y, x - y);
-    printf("%f times %f is %f\n", x, y, x * y);
-    printf("%f divided by %f is %f\n", x, y, x / y);
+    printf("%f times %f is %.2f\n", x, y, x * y);
+    printf("%f divided by %f is %.2f\n", x, y, x / y);
 
 }
