dfe1116ee58b4b9bd2ff6685356d10dfd1787440
Rose Parker <parker.rose@ymail.com>
2018-11-18 22:24:33 +0000

    input from a string and int variable returned in printf

---
+++ hello/hello.c
@@ -0,0 +1,15 @@
+//style50 <nameoffile> to fix style before submitting>
+
+#include<stdio.h>
+#include<cs50.h>
+
+
+int main(void)
+{
+    string name= "Rose";
+    int firstNumber = 5;
+    printf("Hello %s. My number is %i\n", name, firstNumber);
+}
+
+//declares a variabe name and an integer called first number and printing it out
+
